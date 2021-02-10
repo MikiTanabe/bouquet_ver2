c5d36d7799440aac3b534ff1b6d9ef10d4768493
田辺美紀 <tanabemiki@tanabemikinoiMac.local>
2021-02-10 18:06:48 +0900

    firebase追加

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="stickey-top">
+        <div class="container">
+            <div class="col-12 d-flex content-items-center">
+                <div>
+                    <p>イメージコンサルティングのイベント・サロン交流サイト Bouquet</p>
+                    <img src="">
+                </div>
+                <div>
+                    <p><span class="cl-darkpink">▶︎</span><router-link to="mypage">{{ login-str }}</router-link></p>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<script>
+    export default {
+        name: 'Header',
+        computed: {
+            loginStr: function () {
+                if(1) {
+                    return 'ログイン／新規登録'
+                } else {
+                    return 'マイページ'
+                }
+            },
+            urlStr: function () {
+                if(1) {
+                    return 'signin'
+                } else {
+                    return 'home'
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="home-container" class="d-flex">
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+<style scoped>
+#home-container {
+    color: black;
+}
+</style>
